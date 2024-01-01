80202fc975807354ad354bde2b95c5fcf72e241b
Anand jha <codewithann@gmail.com>
2024-01-01 20:05:05 +0530

    call first api

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_rest_api/models/post_model.dart';
+import 'package:http/http.dart' as http;
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,14 +12,85 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  List<PostModel> postList = [];
+  // api method
+  Future<List<PostModel>> getPostApi() async {
+    final response =
+        await http.get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+    var data = jsonDecode(response.body.toString());
+    if (response.statusCode == 200) {
+      for (Map i in data) {
+        postList.add(PostModel.fromJson(i));
+      }
+      return postList;
+    } else {
+      return postList;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        backgroundColor: Color(0xff037aae),
         title: const Text('RestApi'),
       ),
       body: Column(
-        children: [],
+        children: [
+          Expanded(
+            child: FutureBuilder(
+              future: getPostApi(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return const Text(
+                    'Loading....',
+                    style: TextStyle(fontSize: 40, fontWeight: FontWeight.w400),
+                  );
+                } else {
+                  return ListView.builder(
+                    itemCount: postList.length,
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: Card(
+                            child: Padding(
+                          padding: const EdgeInsets.all(12.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text(
+                                "UserId:",
+                                style: TextStyle(
+                                    fontSize: 20, fontWeight: FontWeight.w600),
+                              ),
+                              Text(
+                                postList[index].userId.toString(),
+                              ),
+                              const Text(
+                                "Tittle:",
+                                style: TextStyle(
+                                    fontSize: 18, fontWeight: FontWeight.w600),
+                              ),
+                              Text(postList[index].title.toString()),
+                              const Text(
+                                "Description:",
+                                style: TextStyle(
+                                    fontSize: 20, fontWeight: FontWeight.w600),
+                              ),
+                              Text(
+                                postList[index].body.toString(),
+                              ),
+                            ],
+                          ),
+                        )),
+                      );
+                    },
+                  );
+                }
+              },
+            ),
+          )
+        ],
       ),
     );
   }
