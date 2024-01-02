489fdcba1410d2db91ceec013c8bc414f74897bd
Anand jha <codewithann@gmail.com>
2024-01-02 23:33:04 +0530

    create model and example 3

---
+++ lib/example_three.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ExampleThree extends StatefulWidget {
+  const ExampleThree({super.key});
+
+  @override
+  State<ExampleThree> createState() => _ExampleThreeState();
+}
+
+class _ExampleThreeState extends State<ExampleThree> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Example 3 RestApi'),
+      ),
+      body: Column(
+        children: [],
+      ),
+    );
+  }
+}
